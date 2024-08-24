94a64a69822a67e5820896627d9704e2fb07a455
PexArt-web <pexart74@gmail.com>
2024-08-24 17:03:07 +0100

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  content: [
+    './public/*.html',
+  ],
+  theme: {
+    extend: {
+      colors:{
+        primary: '#ff6363',
+        secondary: {
+          100: '#e2e2d5',
+          200: '#888883'
+        }
+      },
+
+      fontFamily: {
+        body: ['Nunito']
+      }
+    },
+  },
+  plugins: [],
+}
+
